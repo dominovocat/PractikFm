5b29735996ecf37610ecbe3920fd3f7048a36bad
danil <minakonn9shkoni@gmail.com>
2021-12-03 11:08:47 +0300

    Add correction in Header

--- src/PractikFm/Components/Header/index.js
+++ src/PractikFm/Components/Header/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header = (props) => {
   const path = useLocation().pathname;
   return (
     <header>
-      <Link to='/'>
-        <img src='/static/images/squad.png'/>
-        {
-          (path ==='/'|| path === '/signup')?
-          <Link to='/login'>Login</Link>:
-          <Link to='/signup'>signup</Link>
-        }
+      <Link to="/" className={styles.Header}>
+        <img src="/static/images/squad.png" />
+        {path === "/" || path === "/signup" ? (
+          <Link to="/login">Login</Link>
+        ) : (
+          <Link to="/signup">signup</Link>
+        )}
       </Link>
     </header>
   );
-}
+};
 
 export default Header;
-
